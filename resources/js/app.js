e66fc86ae0fe12930ee9ee7dac11000e05251147
Molnár Viktor <molviktor99@gmail.com>
2024-08-22 17:44:47 +0200

    Tailwind, Vue and Frontend module

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+
+import App from "../../Modules/Frontend/resources/assets/js/App.vue";
+
+createApp(App).mount("#app");
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./Modules/**/*.blade.php",
+    "./Modules/**/*.vue",
+  ],
+  theme: {
+  },
+  plugins: [],
+}
+
